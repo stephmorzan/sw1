78ab8c7855b4e1d6582eafc784ae442bcd3bb8e5
Ricardo <pst.falco@gmail.com>
2015-06-16 00:08:03 -0500

    Añadir archivos WEB

---
+++ src/sw1/model/CarpetaComp.java
@@ -0,0 +1,28 @@
+package sw1.model;
+
+/**
+ * Created by Ricardo on 16/06/2015.
+ */
+public class CarpetaComp {
+    int idCarpetaComp;
+    String NombreCarpeta;
+
+    public CarpetaComp() {
+    }
+
+    public int getIdCarpetaComp() {
+        return idCarpetaComp;
+    }
+
+    public void setIdCarpetaComp(int idCarpetaComp) {
+        this.idCarpetaComp = idCarpetaComp;
+    }
+
+    public String getNombreCarpeta() {
+        return NombreCarpeta;
+    }
+
+    public void setNombreCarpeta(String nombreCarpeta) {
+        NombreCarpeta = nombreCarpeta;
+    }
+}
