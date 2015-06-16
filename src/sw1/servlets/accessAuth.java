2c2851002f6bbc4b209f5f45c2ac99b3b2a00af0
Stephy <s.a.m.fuentes08@gmail.com>
2015-06-15 23:31:46 -0500

    Agregar clase servlet accessAuth

---
+++ src/sw1/servlets/accessAuth.java
@@ -0,0 +1,23 @@
+package sw1.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by EQ on 15/06/2015.
+ */
+@WebServlet(name = "accessAuth")
+public class accessAuth extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
